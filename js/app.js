c3dcb2d9b3ad5432a01b53781a10428b60a8de29
Tamas Kalman <ktamas77@gmail.com>
2015-04-09 14:49:01 -0700

    mines can be explored

--- js/app.js
+++ js/app.js
@@ -1,21 +1,54 @@
 // --- build empty game table ---
-function initGameBoard(boardSize) {
-    var gameBoard = {};
-    gameBoard.visitedStyle = {
-        1: 'visited'
-    };
-    gameBoard.size = boardSize;
-    gameBoard.rows = [];
-    for (var y = 0; y < boardSize; y++) {
-        var gameBoardRow = {}
-        gameBoardRow.cells = [];
-        for (var x = 0; x < boardSize; x++) {
-            var cell = {};
-            cell.visited = 0;
-            gameBoardRow.cells.push(cell);
+function initGameBoard(boardSize, maxMines) {
+
+    var gameBoard = {}
+    
+    gameBoard.resetBoard = function(boardSize) {
+        this.size = boardSize;
+        this.rows = [];
+        for (var y = 0; y < boardSize; y++) {
+            var gameBoardRow = {}
+            gameBoardRow.cells = [];
+            for (var x = 0; x < boardSize; x++) {
+                var cell = {
+                    visited: 0,
+                    mine: 0,
+                    cellStyle: 'default',
+                    visit: function() {
+                        this.visited = 1;
+                        if (this.mine == 0) {
+                            this.cellStyle = 'visited';
+                        } else {
+                            this.cellStyle = 'mine';
+                        }
+                    }
+                };
+                gameBoardRow.cells.push(cell);
+            }
+            this.rows.push(gameBoardRow);
         }
-        gameBoard.rows.push(gameBoardRow);
     }
+    
+    gameBoard.getCell = function(x, y) {
+        return this.rows[y].cells[x];
+    };
+
+    gameBoard.addMines = function(maxMines) {
+        var minesAdded = 0;
+        while (minesAdded < maxMines) {
+            x = Math.round(Math.random() * (this.size - 1));
+            y = Math.round(Math.random() * (this.size - 1));
+            cell = this.getCell(x, y);
+            if (cell.mine == 0) {
+                cell.mine = 1;
+                minesAdded++;
+            }
+        }
+    }
+
+    gameBoard.resetBoard(boardSize);
+    gameBoard.addMines(maxMines);
+
     return gameBoard;
 }
 
@@ -24,7 +57,7 @@ function initGameBoard(boardSize) {
     var mineSweeperApp = angular.module('mineSweeperApp', []);
 
     mineSweeperApp.controller('GameboardController', ['$scope', function ($scope) {
-        $scope.gameBoard = initGameBoard(10);
+        $scope.gameBoard = initGameBoard(10, 10);
     }]);
 
 })();
